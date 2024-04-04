fcea04ddd98ec3b8acdb9b2e7bb42395de14b8eb
code_run <908163862@qq.com>
2024-04-04 11:38:52 +0800

    feat: sentinel authorization rule

---
+++ cloudalibaba-sentinel-service8401/src/main/java/org/example/cloud/handler/MyRequestOriginParser.java
@@ -0,0 +1,13 @@
+package org.example.cloud.handler;
+
+import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.RequestOriginParser;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyRequestOriginParser implements RequestOriginParser {
+    @Override
+    public String parseOrigin(HttpServletRequest httpServletRequest) {
+        return httpServletRequest.getParameter("serverName");
+    }
+}
